008bc19527adfbc23812a5e0499a28945ac3a5fa
Ryan Johnson <peacemgeetar16@gmail.com>
2020-12-18 00:52:26 -0500

    Added Pointers for most of the crafting/upgrade locations

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,17 +29,93 @@
         private void InitializeComponent()
         {
             this.butPerk = new System.Windows.Forms.Button();
+            this.butCommonItem = new System.Windows.Forms.Button();
+            this.butUncommon = new System.Windows.Forms.Button();
+            this.butRare = new System.Windows.Forms.Button();
+            this.butRareUp = new System.Windows.Forms.Button();
+            this.butEpicIt = new System.Windows.Forms.Button();
+            this.butEpicUp = new System.Windows.Forms.Button();
+            this.butLegItems = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // butPerk
             // 
-            this.butPerk.Location = new System.Drawing.Point(488, 235);
+            this.butPerk.Location = new System.Drawing.Point(397, 185);
             this.butPerk.Name = "butPerk";
             this.butPerk.Size = new System.Drawing.Size(157, 49);
             this.butPerk.TabIndex = 0;
             this.butPerk.Text = "Perk Point +1";
             this.butPerk.UseVisualStyleBackColor = true;
             this.butPerk.Click += new System.EventHandler(this.butPerk_Click);
+            // 
+            // butCommonItem
+            // 
+            this.butCommonItem.Location = new System.Drawing.Point(397, 299);
+            this.butCommonItem.Name = "butCommonItem";
+            this.butCommonItem.Size = new System.Drawing.Size(157, 49);
+            this.butCommonItem.TabIndex = 1;
+            this.butCommonItem.Text = "Common Item Components +1k";
+            this.butCommonItem.UseVisualStyleBackColor = true;
+            this.butCommonItem.Click += new System.EventHandler(this.butCommonItem_Click);
+            // 
+            // butUncommon
+            // 
+            this.butUncommon.Location = new System.Drawing.Point(397, 412);
+            this.butUncommon.Name = "butUncommon";
+            this.butUncommon.Size = new System.Drawing.Size(157, 49);
+            this.butUncommon.TabIndex = 2;
+            this.butUncommon.Text = "Uncommon Item Components +1k";
+            this.butUncommon.UseVisualStyleBackColor = true;
+            this.butUncommon.Click += new System.EventHandler(this.butUncommon_Click);
+            // 
+            // butRare
+            // 
+            this.butRare.Location = new System.Drawing.Point(397, 527);
+            this.butRare.Name = "butRare";
+            this.butRare.Size = new System.Drawing.Size(157, 49);
+            this.butRare.TabIndex = 3;
+            this.butRare.Text = "Rare Item Components +1k";
+            this.butRare.UseVisualStyleBackColor = true;
+            this.butRare.Click += new System.EventHandler(this.butRare_Click);
+            // 
+            // butRareUp
+            // 
+            this.butRareUp.Location = new System.Drawing.Point(397, 639);
+            this.butRareUp.Name = "butRareUp";
+            this.butRareUp.Size = new System.Drawing.Size(157, 49);
+            this.butRareUp.TabIndex = 4;
+            this.butRareUp.Text = "Rare Upgrade Components +1k";
+            this.butRareUp.UseVisualStyleBackColor = true;
+            this.butRareUp.Click += new System.EventHandler(this.butRareUp_Click);
+            // 
+            // butEpicIt
+            // 
+            this.butEpicIt.Location = new System.Drawing.Point(637, 185);
+            this.butEpicIt.Name = "butEpicIt";
+            this.butEpicIt.Size = new System.Drawing.Size(157, 49);
+            this.butEpicIt.TabIndex = 5;
+            this.butEpicIt.Text = "Epic Item Components +1k";
+            this.butEpicIt.UseVisualStyleBackColor = true;
+            // 
+            // butEpicUp
+            // 
+            this.butEpicUp.Location = new System.Drawing.Point(637, 299);
+            this.butEpicUp.Name = "butEpicUp";
+            this.butEpicUp.Size = new System.Drawing.Size(157, 49);
+            this.butEpicUp.TabIndex = 6;
+            this.butEpicUp.Text = "Epic Upgrade Components +1k";
+            this.butEpicUp.UseVisualStyleBackColor = true;
+            this.butEpicUp.Click += new System.EventHandler(this.butEpicUp_Click);
+            // 
+            // butLegItems
+            // 
+            this.butLegItems.Location = new System.Drawing.Point(637, 412);
+            this.butLegItems.Name = "butLegItems";
+            this.butLegItems.Size = new System.Drawing.Size(157, 49);
+            this.butLegItems.TabIndex = 7;
+            this.butLegItems.Text = "Legendary Item Components +1k";
+            this.butLegItems.UseVisualStyleBackColor = true;
+            this.butLegItems.Click += new System.EventHandler(this.butLegItems_Click);
             // 
             // Form1
             // 
@@ -48,6 +124,13 @@
             this.BackgroundImage = global::Cyber_Punk_Trainer.Properties.Resources.thumb_1920_1062752;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1505, 824);
+            this.Controls.Add(this.butLegItems);
+            this.Controls.Add(this.butEpicUp);
+            this.Controls.Add(this.butEpicIt);
+            this.Controls.Add(this.butRareUp);
+            this.Controls.Add(this.butRare);
+            this.Controls.Add(this.butUncommon);
+            this.Controls.Add(this.butCommonItem);
             this.Controls.Add(this.butPerk);
             this.Name = "Form1";
             this.Text = "Trainer Punk";
@@ -58,6 +141,13 @@
         #endregion
 
         private System.Windows.Forms.Button butPerk;
+        private System.Windows.Forms.Button butCommonItem;
+        private System.Windows.Forms.Button butUncommon;
+        private System.Windows.Forms.Button butRare;
+        private System.Windows.Forms.Button butRareUp;
+        private System.Windows.Forms.Button butEpicIt;
+        private System.Windows.Forms.Button butEpicUp;
+        private System.Windows.Forms.Button butLegItems;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,76 +11,23 @@ using System.Threading;
 using Memory;
 
 namespace Cyber_Punk_Trainer
-{
+{     
     public partial class Form1 : Form
     {
         Mem Meme = new Mem();
         public static string CyberPunk = "Cyberpunk2077.exe";
-        public static string PerkPoints = "Cyberpunk2077.exe+0x03CBF400,0x9B0,0x38,0x0,0x0,0x38,0x68,0x14";
-
+        public static string PerkPoints = "Cyberpunk2077.exe+0x03CBF400,0x9B0,0x38,0x0,0x0,0x38,0x68,0x14"; //Perk Points
+        public static string CommonCraftingItem = "Cyberpunk2077.exe+049221B8,0x260,0x18,0x5F0,0x58,0x0,0x0,0x120,0x78"; //Common Item Components
+        public static string UncommonCraftingItem = "Cyberpunk2077.exe+0x03CBF400,0xF60,0x50,0x38,0x280,0xF0,0x0,0x130,0x78"; //Uncommon Item Component
+        public static string RareCraftingItem = "Cyberpunk2077.exe+0x04921BB8,0x40,0x38,0x2F0,0x58,0x0,0x0,0x310,0x78"; //Rare Item Components
+        public static string RareUpgradeItem = "Cyberpunk2077.exe+0x04637A50,0x240,0x18,0x120,0xB0,0xF0,0x0,0xA80,0x108"; //Rare Upgrade Components
+        public static string EpicItemComponents = "Cyberpunk2077.exe+0x04921BB8,0x118,0x50,0x120,0xB0,0xF0,0x0,0xB00,0x738"; //Epic Item Components
+        public static string EpicUpgradeComponents = "Cyberpunk2077.exe+0x04921BB8,0x198,0x80,0x120,0xB0,0xF0,0x0,0x9A0,0x108"; //Epic Upgrade Components
+        public static string LegendaryItemComponents = "Cyberpunk2077.exe+0x04922100,0x480,0x18,0x5F0,0x58,0x0,0x0,0x310,0x588"; //Legendary Item Components
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void butPerk_Click(object sender, EventArgs e)
-        {
-            int PID = Meme.GetProcIdFromName("Cyberpunk2077.exe");
-            if (PID > 0)
-            {
-                Meme.OpenProcess(PID);
-
-                Thread PPA = new Thread(addPerk) { IsBackground = true };
-                PPA.Start();
-                
-            }
-        }
-
-
-
-        /// <summary>
-        /// This adds a specified value to a stat of your choosing, provided you've found the value in memory
-        /// </summary>
-        /// <param name="pointer"> The string to reach memory (pointer/offsets),
-        /// i.e (PerkPoints = "Cyberpunk2077.exe+0x03CBF400,0x9B0,0x38,0x0,0x0,0x38,0x68,0x14")</param>
-        /// <param name="amount">The amount (int) to increase stat</param>
-        /// <param name="stat">The name of the stat in string format</param>
-        private void statAdd(string pointer, int amount, string stat)
-        {
-            try
-            {
-                int currentStat = Meme.ReadInt(pointer);
-                int newStat = currentStat + amount;
-                Meme.WriteMemory(pointer, "int", newStat.ToString());
-                Thread.Sleep(2);
-                MessageBox.Show("You now have " + newStat.ToString() + " " + stat);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
-        }
-
-
-
-        /// <summary>
-        /// This method makes any methods in the parameter, a background, threaded task.
-        /// </summary>
-        /// <param name="method"> Takes in a method that edits memory
-        /// i.e (addPerk()) Which, will add 1 point to the perk stat</param>
-        private void CPThreader(System.Threading.ThreadStart method)
-        {
-            int PID = Meme.GetProcIdFromName(CyberPunk);
-            if (PID > 0)
-            {
-                Meme.OpenProcess(PID);
-                Thread Thrd = new Thread(method) { IsBackground = true };
-                Thrd.Start();
-            }
-        }
-
-
-
 
         private void addPerk()
         {
@@ -97,6 +44,111 @@ namespace Cyber_Punk_Trainer
             {
                 MessageBox.Show(e.Message);
                 Thread.Sleep(2);
+            }
+        }
+
+
+
+
+
+        /// <summary>
+        /// This adds a specified value to a stat of your choosing, provided you've found the value in memory
+        /// </summary>
+        /// <param name="pointer"> The string to reach memory (pointer/offsets),
+        /// i.e (PerkPoints = "Cyberpunk2077.exe+0x03CBF400,0x9B0,0x38,0x0,0x0,0x38,0x68,0x14")</param>
+        /// <param name="amount">The amount (int) to increase stat</param>
+        /// <param name="stat">The name of the stat in string format</param>
+        private void statAdd(string pointer, int amount, string stat)
+        {
+            try
+            {
+                int currentStat = Meme.ReadInt(pointer);
+                if (currentStat == 0) { MessageBox.Show("Value may be 0/invalid!"); }
+                int newStat = currentStat + amount;
+                Meme.WriteMemory(pointer, "int", newStat.ToString());
+                Thread.Sleep(2);
+                MessageBox.Show("You now have " + newStat.ToString() + " " + stat);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        //Button to add 1k to uncommon item components
+        private void butUncommon_Click(object sender, EventArgs e)
+        {
+            {
+                int PID = Meme.GetProcIdFromName(CyberPunk);
+                if (PID > 0)
+                {
+                    Meme.OpenProcess(PID);
+                    // This method makes any methods in the parameter, a background, threaded task.
+                    Thread t = new Thread(() => statAdd(UncommonCraftingItem, 1000, "Uncommon Crafting Items")) { IsBackground = true };
+                    t.Start();
+
+                }
+            }
+        }
+
+        //Button to add 1k to rare item components
+        private void butRare_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Button to add 1 available perk point
+        private void butPerk_Click(object sender, EventArgs e)
+        {
+            int PID = Meme.GetProcIdFromName("Cyberpunk2077.exe");
+            if (PID > 0)
+            {
+                Meme.OpenProcess(PID);
+
+                Thread PPA = new Thread(addPerk) { IsBackground = true };
+                PPA.Start();
+
+            }
+        }
+
+        //Button to add 1k to common item components
+        private void butCommonItem_Click(object sender, EventArgs e)
+        {
+            int PID = Meme.GetProcIdFromName(CyberPunk);
+            if (PID > 0)
+            {
+                Meme.OpenProcess(PID);
+                // This method makes any methods in the parameter, a background, threaded task.
+                Thread t = new Thread(() => statAdd(CommonCraftingItem, 1000, "Common Item Components")) { IsBackground = true };
+                t.Start();
+            }
+        }
+
+        //Button to add 1k to rare upgrade components
+        private void butRareUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Button to add 1k to epic upgrade components
+        private void butEpicUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Button to add 1k to legendary item components
+        private void butLegItems_Click(object sender, EventArgs e)
+        {
+            {
+                int PID = Meme.GetProcIdFromName(CyberPunk);
+                if (PID > 0)
+                {
+                    Meme.OpenProcess(PID);
+                    // This method makes any methods in the parameter, a background, threaded task.
+                    Thread t = new Thread(() => statAdd(LegendaryItemComponents, 1000, "Legendary Crafting Items")) { IsBackground = true };
+                    t.Start();
+
+                }
             }
         }
     }
